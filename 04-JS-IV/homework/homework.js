1937283b0c0b77a6526657cb96cd120960c42dc8
DISAN - HENRY SEBASTIAN BELALCAZAR MURCIA <HS.BELALCAZAR@policia.gov.co>
2022-11-11 16:46:36 -0500

    mas y ahi vamos

--- 04-JS-IV/homework/homework.js
+++ 04-JS-IV/homework/homework.js
@@ -6,6 +6,15 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var obj= {
+    nombre:nombre,
+    edad:edad,
+    meow: function() {
+      return "Meow!";
+    }
+  };
+  return obj;
+
 }
 
 function agregarPropiedad (objeto, property) {
@@ -13,6 +22,9 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto[property] = null;
+  return objeto;
+
 }
 
 function invocarMetodo (objeto, metodo) {
